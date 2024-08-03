79e8d087b1f909feaf608d39032182972ec4aa9c
Hamids29 <shah.hamids@outlook.com>
2024-08-03 19:38:31 -0400

    finished inventory management system base

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { Box, Modal, TextField, Typography,Stack, Button } from "@mui/material";
 import { firestore } from "@/firebase";
-import {collection, getDocs, query} from "firebase/firestore";
+import {collection, deleteDoc, doc, getDocs, query, getDoc, setDoc} from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -124,7 +124,7 @@ onClick={() =>{
   Inventory Items
 </Typography>
   </Box>
- </Box>
+
  <Stack width={"800px"} height={"300px"} spacing={2} overflow={"auto"}>
   {inventory.map(({name, quantity})=>(
       <Box
@@ -133,20 +133,38 @@ onClick={() =>{
       minHeight={"150px"}
       display= "flex"
       alignItems={"center"}
-      justifyContent={"center"}
+      justifyContent={"space-between"}
       bgcolor={"f0f0f0"}
       padding={5}
       >
-  <Typography 
-  variant='h3'
-  color={"#333"} 
-  textAlign={"center"}
-  >
+  <Typography variant='h3' color={"#333"}  textAlign={"center"} >
     {name.charAt(0).toUpperCase() + name.slice(1)}
   </Typography>
+  <Typography variant='h3' color={"#333"}  textAlign={"center"} >
+    {quantity}
+  </Typography>
+  <Stack direction={"row"} spacing={2}>
+  <Button 
+  variant="contained"
+  onClick={() => {
+    addItem(name)
+  }}
+>
+    Add
+  </Button>
+  <Button 
+  variant="contained"
+  onClick={() => {
+    removeItem(name)
+  }}
+>
+    Remove
+  </Button>
+  </Stack>
    </Box>
     )) }
  </Stack>
+ </Box>
 </Box>
   )
 }
