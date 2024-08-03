312d39821b9ae83c3f8950f9e0dfa58a06914401
Hamids29 <shah.hamids@outlook.com>
2024-08-03 19:46:59 -0400

    color changes

--- app/page.js
+++ app/page.js
@@ -62,6 +62,7 @@ export default function Home() {
 
   return(
    <Box width={"100vw"} 
+
    height={"100vh"} 
    display={"flex"} 
    flexDirection="column"
@@ -165,6 +166,7 @@ onClick={() =>{
     )) }
  </Stack>
  </Box>
+
 </Box>
   )
 }
